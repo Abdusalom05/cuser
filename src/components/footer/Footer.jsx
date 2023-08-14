b277d94fe42a0b2d671417be55fa9d0f2814efd1
Abdusalom <93870816+Abdusalom05@users.noreply.github.com>
2023-08-14 23:22:08 +0500

    Add files via upload

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,16 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <div className="footer-container container">
+        <p>
+          All Right Reserved.{" "}
+          <a href="https://t.me/Sharifov_Abdusalom">Sharifov Abdusalom</a>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
